52f039472ceccf1914665f225ab59df6a5e9cf3c
SalmanBhagat <mrsalman9222@gmail.com>
2025-09-17 16:08:24 +0530

    update code

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -46,6 +46,7 @@ export default function Login() {
       } else if (errorCode == "auth/wrong-password") {
         toast.error("Incorrect password")
       } else{
+        // test
         toast.error("Login Failed!")
       }
     })
